da0c7fc88dee3cb73574512468e4c1038dd851bf
2eelee <pipi2938@naver.com>
2026-06-11 21:34:43 +0900

    Initial Commit

---
+++ Source/DroneProto/DroneProto.Build.cs
@@ -0,0 +1,51 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class DroneProto : ModuleRules
+{
+	public DroneProto(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"DroneProto",
+			"DroneProto/Variant_Platforming",
+			"DroneProto/Variant_Platforming/Animation",
+			"DroneProto/Variant_Combat",
+			"DroneProto/Variant_Combat/AI",
+			"DroneProto/Variant_Combat/Animation",
+			"DroneProto/Variant_Combat/Gameplay",
+			"DroneProto/Variant_Combat/Interfaces",
+			"DroneProto/Variant_Combat/UI",
+			"DroneProto/Variant_SideScrolling",
+			"DroneProto/Variant_SideScrolling/AI",
+			"DroneProto/Variant_SideScrolling/Gameplay",
+			"DroneProto/Variant_SideScrolling/Interfaces",
+			"DroneProto/Variant_SideScrolling/UI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/DroneProto/DroneProto.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DroneProto.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DroneProto, "DroneProto" );
+
+DEFINE_LOG_CATEGORY(LogDroneProto)
